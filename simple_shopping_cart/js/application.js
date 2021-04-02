b44c484131e1ee5f96b6d809d2e220b9254236f6
Kevin Venegas <kevinv234@gmail.com>
2021-04-01 21:42:46 -0700

    Removing broken sum function

--- simple_shopping_cart/js/application.js
+++ simple_shopping_cart/js/application.js
@@ -2,38 +2,7 @@
 
 $(document).ready(function() {
 
-	var sum = function() {
-		// collet div of all item-qty and store in array qtys
-		// var qtys = $('#item-list .item-qty input');
-		var prices = $('.item-price');
-		var qtys = $('.quantity');
-			// qtys.splice(9,1);
-			total = 0;
 	
-		for (i=0; i<qtys.length; i++) {
-		  // convert the value of each element in qtys into number and
-		  // multiply by corresponding item price, then add to sum
-		  var price = Number($(prices[i]).text().replace(/\$/,""));
-		  var subtotal = (Number($(qtys[i]).val())) * price;
-		  if (subtotal != 0) {
-			$($('.item-subtotal')[i]).text("$" + subtotal);
-		  } else {
-			$($('.item-subtotal')[i]).text("$--.--");
-		  }
-		  total += subtotal    
-		}
-		$('#total-price').text("$ " + total);
-		var addspace = "";
-		var spaces = total.toString();
-			spaces = spaces.length;
-			spaces = 12 - spaces;
-			for (i=0; i<spaces; i++) {
-			  addspace += " ";
-			}
-	
-		return total;
-	  }
-
 	var addItem = function(name,cost) {
 		name = name.charAt(0).toUpperCase() + name.slice(1);
 		$('#item-list').prepend('<div class="row item"> \
