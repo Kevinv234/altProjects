bce0ad01c73bf7824c1bc587fc9b8645043287f3
Kevin Venegas <kevinv234@gmail.com>
2021-04-01 21:37:10 -0700

    modifying the classes of the bootstrap classes for the items being added

--- simple_shopping_cart/js/application.js
+++ simple_shopping_cart/js/application.js
@@ -37,13 +37,13 @@ $(document).ready(function() {
 	var addItem = function(name,cost) {
 		name = name.charAt(0).toUpperCase() + name.slice(1);
 		$('#item-list').prepend('<div class="row item"> \
-		  <div class="name col-xs-3"> \ '
+		  <div class="name col-s-1"> \ '
 		  +  name + '\
 		  </div> \
-		  <div class="price col-xs-3"> \
+		  <div class="price col-s-1"> \
 			$' + cost + '.00 \
 		  </div> \
-		  <div class="item-qty col-xs-3"> \
+		  <div class="item-qty col-s-1"> \
 			<label>QTY</label> \
 			<input class="quantity" type="number"> \
 		  </div> \
