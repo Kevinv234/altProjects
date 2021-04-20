fe652065f371a978f4345fd68d421c2b385cacc4
Kevin Venegas <kevinv234@gmail.com>
2021-04-19 22:15:49 -0700

    Adding changes that were requested to the sum function

--- simple_shopping_cart/js/application.js
+++ simple_shopping_cart/js/application.js
@@ -1,15 +1,51 @@
 
 
 $(document).ready(function() {
+  var total = 0;
 
+  var sum = function() {
+
+	//price and quantity will be multiplied. 
+    var prices = $('.item-price');
+    var qtys = $('.quantity');
+    total = 0;
 	
+	//loop through quanities and multiply the price the the amount
+    for (i=0; i<qtys.length; i++) {
+
+      var price = Number($(prices[i]).text().replace(/\$/,""));
+      var subtotal = (Number($(qtys[i]).val())) * price;
+
+		
+	  //if it is not zero, add the replace the empyty text
+      if (subtotal != 0) {
+        $($('.item-subtotal')[i]).text("$" + subtotal);
+      } else {
+        $($('.item-subtotal')[i]).text("$--.--");
+      }
+      total += subtotal    
+    }
+
+	//Display total price
+    $('#total-price').text("$ " + total);
+
+    var addspace = "";
+    var spaces = total.toString();
+        spaces = 12 - spaces.length;
+        for (i=0; i<spaces; i++) {
+          addspace += " ";
+        }
+
+    return total;
+  }
+  
 	var addItem = function(name,cost) {
 		name = name.charAt(0).toUpperCase() + name.slice(1);
 		$('#item-list').prepend('<div class="row item"> \
-		  <div class="name col-s-1"> \ '
+		  <div class="item-name col-xs-3"> \ '
 		  +  name + '\
 		  </div> \
-		  <div class="price col-s-1"> \
+		  <div class="item-price col-xs-3"> \
 			$' + cost + '.00 \
 		  </div> \
 		  <div class="item-qty col-s-1"> \
@@ -27,18 +63,67 @@ $(document).ready(function() {
 		</div>');
 	}
 
+	var sortItem = function() {
+		var prices = $('.item-price');
+		var names = $('.item-name');
+		var priceName = [];
+
+		for (i=0; i<prices.length; i++) {
+		  var price = $(prices[i]).text().trim();
+		  var name = $(names[i]).text().trim();
+		  priceName.push([name,price]);
+		} 
+
+		priceName.sort();
+
+
+
+
+		for (i=0; i<priceName.length; i++) {
+		  $($('.item-name')[i]).text(priceName[i][0]);
+		  $($('.item-price')[i]).text(priceName[i][1]);
+		}
+	  }
+	
+	
 	$(document).on('click', '.remove', function() {
 		$(this).parents('.row').remove();
 		sum();
 	});
 
-	$(document).on('click', '#fork', function() {
-		addItem($('#name').val(), $('#cost').val());
-	});
-
-
 	$(document).on('click', '.sum', function() {
 		sum();
 	});
 
+
+	$(document).on('click', '#sort', function() {
+		sortItem();
+	  });
+	
+	  $(document).on('click', '#fork', function() {
+		addItem($('#name').val(), $('#cost').val());
+	  });
+	
+	  $(document).on('click', '.remove', function() {
+		$(this).parents('.row').remove();
+		sum();
+	  });
+	
+	  $('input').keydown(function(e){
+		// console.log(e.which);
+		if (e.which == 13) {
+		  sum();
+		};
+	  });
+	
+	  $(document).on('keyup', '.quantity', function(){
+		sum();
+	  });
+	
+	  $(document).on('keydown', '#cost', function(e){
+		// console.log(e.which);
+		if (e.which == 13) {
+		  addItem($('#name').val(), $('#cost').val());
+		};
+	  });
 });
